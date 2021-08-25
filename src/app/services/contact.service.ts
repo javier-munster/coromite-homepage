078bcf948ce15c90218430d212af038ef9df2b5f
Javier Munster <javier.munster@gmail.com>
2021-08-24 22:06:39 -0400

    chg: added http headers to request

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ContactUsForm } from "../models/contact-us-form";
 
@@ -7,10 +7,15 @@ import { ContactUsForm } from "../models/contact-us-form";
 })
 export class ContactService {
     private readonly COROMITE_EMAIL_ENDPOINT = "https://www.coromite.com/contact-us";
+    readonly httpOptions = {
+        headers: new HttpHeaders({
+            "Content-Type": "application/json"
+        })
+    }
 
     constructor(private http: HttpClient) { }
 
     postMessage(data: ContactUsForm) {
-        return this.http.post(this.COROMITE_EMAIL_ENDPOINT, data);
+        return this.http.post(this.COROMITE_EMAIL_ENDPOINT, data, this.httpOptions);
     }
 }
