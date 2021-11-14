1eb5186611744cbcef2efa271850ab47443dab64
Javier Munster <javier.munster@gmail.com>
2021-11-14 18:10:59 -0500

    new: initial scully installation

---
+++ scully.coromite-homepage.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from "@scullyio/scully";
+export const config: ScullyConfig = {
+    projectRoot: "./src",
+    projectName: "coromite-homepage",
+    outDir: "./dist/static",
+    routes: {
+    }
+};
